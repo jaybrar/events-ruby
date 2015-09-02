0ff931f5380005ed7260f8cac8e96eb072a19e8f
Jay Brar <mandeepbrar@Jays-MacBook-Pro.local>
2015-09-01 19:17:05 -0700

    dsf

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,15 +10,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :domain               => 'gmail.com',
-   :user_name            => 'jaybrar79@gmail.com',
-   :password             => 'brar4463',
-   :authentication       => "plain",
-   :enable_starttls_auto => true
-  }
+  # config.action_mailer.smtp_settings = {
+  #  :address              => "smtp.gmail.com",
+  #  :port                 => 587,
+  #  :domain               => 'gmail.com',
+  #  :user_name            => 'jaybrar79@gmail.com',
+  #  :password             => 'brar4463',
+  #  :authentication       => "plain",
+  #  :enable_starttls_auto => true
+  # }
   # Do not eager load code on boot.
   config.eager_load = false
 
