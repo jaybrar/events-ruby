81526e92b38badd05c9d9003fdcaeae831e53112
Jay Brar <mandeepbrar@Jays-MacBook-Pro.local>
2015-09-01 19:18:32 -0700

    dsf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,16 +7,16 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   # SMTP settings for gmail
-  # config.action_mailer.smtp_settings = {
-  #  :address              => "smtp.gmail.com",
-  #  :port                 => 587,
-  #  :domain               => 'gmail.com',
-  #  :user_name            => 'jaybrar79@gmail.com',
-  #  :password             => 'brar4463',
-  #  :authentication       => "plain",
-  #  :enable_starttls_auto => true
-  # }
-  # # Eager load code on boot. This eager loads most of Rails and
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :domain               => 'gmail.com',
+   :user_name            => 'jaybrar79@gmail.com',
+   :password             => 'brar4463',
+   :authentication       => "plain",
+   :enable_starttls_auto => true
+  }
+  # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
