f9d537f5281fa21021720ab791bf15b94408e058
Jay Brar <mandeepbrar@Jays-MacBook-Pro.local>
2015-09-01 18:16:58 -0700

    csdh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,19 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :domain               => 'gmail.com',
+   :user_name            => 'jaybrar79@gmail.com',
+   :password             => 'brar6032',
+   :authentication       => "plain",
+   :enable_starttls_auto => true
+  }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
