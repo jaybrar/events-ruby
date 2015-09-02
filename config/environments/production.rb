927f2d19f09a5dc223c926435533e6d32d70829c
Jay Brar <mandeepbrar@Jays-MacBook-Pro.local>
2015-09-01 19:42:18 -0700

    dsf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
-   :port                 => 587,
+   :port                 => 25,
    :domain               => 'gmail.com',
    :user_name            => 'jaybrar79@gmail.com',
    :password             => 'brar4463',
