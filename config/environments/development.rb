0d52adc72f7b78e475632119e93f19ae4ccd31ef
Jay Brar <mandeepbrar@Jays-MacBook-Pro.local>
2015-09-01 19:11:13 -0700

    sdfas

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,19 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :domain               => 'gmail.com',
+   :user_name            => 'jaybrar79@gmail.com',
+   :password             => 'brar4463',
+   :authentication       => "plain",
+   :enable_starttls_auto => true
+  }
   # Do not eager load code on boot.
   config.eager_load = false
 
