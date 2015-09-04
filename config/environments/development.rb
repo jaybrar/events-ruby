96a0e36610e47328c742a705b6023b0caf21d181
Jay Brar <mandeepbrar@Jays-MacBook-Pro.local>
2015-09-04 10:32:08 -0700

    changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
    :port                 => 25,
    :domain               => '52.88.76.73:80',
    :user_name            => 'jaybrar79@gmail.com',
-   :password             => 'brar4463',
+   :password             => 'your password',
    :authentication       => "plain",
    :enable_starttls_auto => true
   }
